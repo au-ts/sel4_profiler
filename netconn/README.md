26251765abc9d96b187489e076ff1fff42c52b6e
Ivan Velickovic <i.velickovic@unsw.edu.au>
2024-01-12 17:08:55 +1100

    Rename network_host to netconn and clean up script/README

---
+++ netconn/README.md
@@ -0,0 +1,12 @@
+# Network Host
+
+This is a basic python program to connect to the seL4 Profiling Framework,
+and collects samples over the network to store in JSON format.
+These samples will be stored in the `samples.json` file.
+
+# Usage
+
+```
+python3 netconn.py --ip <TARGET_IP> --port <TARGET_PORT>
+```
+
--- netconn/netconn.py
+++ netconn/netconn.py
@@ -21,12 +21,12 @@ class ProfilerClient:
     will also recieve samples on this client from the seL4 profiler.
     """
 
-    def __init__(self, hostname, port):
+    def __init__(self, output, hostname, port):
+        self.output = open(output, "w")
         self.hostname = hostname
         self.port = port
         self.socket = None
         self.recv_thread = None
-        self.f = None
 
     def send_command(self, cmd):
         """
@@ -54,15 +54,15 @@ class ProfilerClient:
         f.write("}")
     def connect(self):
         self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        
+
         try:
             self.socket.connect((self.hostname, self.port))
         except OSError:
             sys.stderr.write(f"Can't connect to {self.hostname} on port {repr(self.port)}"
                         + " ... invalid host or port?\n")
             sys.exit(1)
-        
-        
+
+
         data = self.socket.recv(1024).decode()
         print(str(data))
 
@@ -83,7 +83,7 @@ class ProfilerClient:
                 # print(f"This is len: {len}")
                 global first_msg
                 if (first_msg == 1):
-                    self.f.write(",")
+                    self.output.write(",")
                 else:
                     first_msg = 1
 
@@ -92,7 +92,7 @@ class ProfilerClient:
                 sample = pmu_sample_pb2.pmu_sample()
 
                 sample.ParseFromString(data)
-                self.f.write(MessageToJson(sample, including_default_value_fields=True))
+                self.output.write(MessageToJson(sample, including_default_value_fields=True))
 
             except socket.error:
                 global stop_recv
@@ -103,14 +103,13 @@ class ProfilerClient:
                     # Dummy command to refresh socket
                     self.send_command("REFRESH")
 
-    def recv_samples(self, f):
+    def recv_samples(self):
         # Start the samples section of the json
         self.socket.settimeout(2)
         global stop_recv
         stop_recv = 0
         self.recv_thread = threading.Thread(target=self.recv_samples_thread, args=())
         self.recv_thread.daemon = True
-        self.f = open("samples.json", "a")
         self.recv_thread.start()
 
     def stop_samples(self):
@@ -118,7 +117,6 @@ class ProfilerClient:
         stop_recv = 1
         # time.sleep(10)
         self.recv_thread.join()
-        self.f.close()
 
 
 if __name__ == "__main__":
@@ -126,13 +124,13 @@ if __name__ == "__main__":
     parser.add_argument('--ip', dest='client_ip', required='true', help='IP address of the target system')
     parser.add_argument('--port', dest='client_port', required='true', help='Port of the target system',
                         type=int)
-    args = parser.parse_args()  
+    parser.add_argument('--output', dest='output', help='Port of the target system',
+                        default="samples.json")
+    args = parser.parse_args()
 
     # Intially, we want to create client object
-    profClient = ProfilerClient(args.client_ip, args.client_port)
-    print("seL4 Profiler Network Client. Please enter command:")
-
-    f = None
+    client = ProfilerClient(args.output, args.client_ip, args.client_port)
+    print("netconn. Please enter command:")
 
     # Create the recv thread. Start on START command, exit on STOP command.
 
@@ -140,44 +138,40 @@ if __name__ == "__main__":
     while True:
         user_input = input("> ").upper()
         if user_input == "CONNECT":
-            f = open("samples.json", "w")
-            profClient.connect()
-            profClient.get_mappings(f)
-            f.write(",\n\"samples\": [\n")
-            f.close()
+            client.connect()
+            client.get_mappings()
+            client.output.write(",\n\"samples\": [\n")
 
         elif user_input == "START":
-            f = open("samples.json", "a")
-            profClient.send_command("START")
-            # TODO: Start this in a seperate thread
-            # recvThread = threading.Thread(target=profClient.recv_samples, args=(f))
+            if client.socket is not None:
+                client.send_command("START")
+                # TODO: Start this in a seperate thread
+                # recvThread = threading.Thread(target=client.recv_samples, args=(f))
 
-            profClient.recv_samples(f)
-            # recvThread.start()
+                client.recv_samples(f)
+                # recvThread.start()
+            else:
+                print("ERROR: attempted START command before CONNECT command called")
 
         elif user_input == "STOP":
-            # Stop the recv thread and close file descriptor
-            profClient.send_command("STOP")
-            profClient.stop_samples()
+            if client.socket is not None:
+                # Stop the recv thread and close file descriptor
+                client.send_command("STOP")
+                client.stop_samples()
+            else:
+                print("ERROR: attempted STOP command before CONNECT command called")
 
         elif user_input == "EXIT":
-            profClient.send_command("STOP")   
-            profClient.stop_samples()
-            f = open("samples.json", "a")
-            f.write("]\n}\n")
-            f.close()
-            os._exit(1)
-        
-        else: 
+            if client.socket is not None:
+                client.send_command("STOP")
+                client.stop_samples()
+                client.output.write("]\n}\n")
+                client.output.close()
+            exit(0)
+
+        else:
             print("These are the following valid commands (case-agnostic):\n")
             print("\t1. \"CONNECT\" - This will attempt to connect to the supplied IP address and port. Connect will add the ")
-            print("\t2. \"START\" - This will command the seL4 profiler to start measurements and construct samples.json file.")
+            print("\t2. \"START\" - This will command the seL4 profiler to start measurements and construct samples output file.")
             print("\t3. \"STOP\" - This will command the seL4 profiler to stop measurements.")
-            print("\t4. \"EXIT\" - This will command the seL4 profiler to stop measurements, close the json file and exit this program")
-
-
-
-
-
-
-
+            print("\t4. \"EXIT\" - This will command the seL4 profiler to stop measurements, close the samples output file and exit this program")
