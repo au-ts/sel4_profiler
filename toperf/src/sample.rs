f7999408037df33865083e35172ab7da61541313
Krishnan Winter <krishnanwinter1@gmail.com>
2024-02-07 15:19:20 +1100

    toperf: Compatible with protobuf changes, where toJson formats 64 bit variables to strings for javascript precision reasons.

--- toperf/src/sample.rs
+++ toperf/src/sample.rs
@@ -1,25 +1,31 @@
-use serde::{Serialize, Deserialize};
+use serde_with::{serde_as, DisplayFromStr};
+use serde_aux::prelude::*;
 
 pub const CALL_STACK_DEPTH: usize = 4;
 
 #[repr(C)]
-#[derive(Debug, Serialize, Deserialize)]
+#[serde_as]
+#[derive(Debug, serde::Serialize, serde::Deserialize)]
 pub struct Sel4Sample {
     // instruction pointer
+    #[serde(deserialize_with = "deserialize_number_from_string")]
     pub ip: u64,
 
     // protection domain id
     pub pd: u32,
 
     // timestamp of when the sample occured
+    #[serde(deserialize_with = "deserialize_number_from_string")]
     pub timestamp: u64,
 
     // cpu affinity - which cpu is being used
     pub cpu: u32,
 
     // number of events per sample
+    #[serde(deserialize_with = "deserialize_number_from_string")]
     pub period: u64,
 
-    // // call stack - provides a trace of addresses for functions called
+    // call stack - provides a trace of addresses for functions called
+    #[serde_as(as = "[DisplayFromStr; CALL_STACK_DEPTH]")]
     pub ips: [u64; CALL_STACK_DEPTH],
 }
